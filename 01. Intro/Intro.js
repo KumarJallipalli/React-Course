d49bae7ad8746ececa11c9962e5aa8c8424f82a9
Siva Jallipalli <kumar.jallipalli@gmail.com>
2024-05-31 12:30:35 +0530

    React Intro

---
+++ 01. Intro/Intro.js
@@ -0,0 +1,49 @@
+/**
+
+Why to learn React..?
+
+    - Is it for getting a job..?
+    - As React is the market trend & got so much hype, Hence are we learning react..?
+        - → cuzz tomorrow some new tchnology will be market trend & will get hyped
+    - Is it to build UI..?
+        - → cuz we can build using HTML, CSS & JS (or) even AngularJS and so on..
+    - No..!
+    - It makes easy to manage & build Complex FrontEnd
+
+
+
+Why React was created..?
+
+    - FB has a problem called Phanthom/Ghost Messaging Problem
+    - Phanthom/Ghost Messaging  → Notifications shows unread messages even when we don’t have any.
+    - Here, state is managed by JS & UI is managed by DOM. And both of these are NOT in sync hence caused this Ghost messages
+    - To solve this problem → React is created
+
+
+
+What is React..?
+
+    - React is a JavaScript library for building user interfaces based on components
+        - React is NOT a Framework but a Library.
+
+
+
+React Drawbacks
+
+    - React is NOT a complete solution in most cases
+        - React doesn’t have Router
+        - React doesn’t have State Management
+        - React has NO SEO by default
+        - React code is rendered back to JS, HTML & CSS → which is called compiling & is a heavy work
+
+
+
+Difference between a Library & Framework..?
+
+    - Both libraries and frameworks are collections of pre-written code that developers can utilize to speed up the application development process
+    - The key technical difference between a framework and library lies in a term called "inversion of control".
+        - When you use a library, you are in charge of the flow of the application
+            - You are choosing when and where to call the library. [we plug in the library code in our own code]
+        - When you use a framework, the framework is in charge of the flow
+            - It provides some places for you to plug in your code, but it calls the code you plugged in as needed
+*/
